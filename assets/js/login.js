13ceff7fcc27f53b087498fe1bf3886a36a22521
yucai666 <389279956@qq.com>
2021-01-21 11:20:36 +0800

    完成了login和regester

---
+++ assets/js/login.js
@@ -0,0 +1,70 @@
+$(function(){
+    // 点击“去注册账号”的链接
+    $('#link_reg').on('click',function(){
+        $('.login-box').hide();
+        $('.reg-box').show();
+    })
+    // 点击“登录账号”的链接
+    $('#link_login').on('click',function(){
+        $('.reg-box').hide();
+        $('.login-box').show();
+    })
+    // 验证密码框
+    var form = layui.form;
+    form.verify({
+        // 自定义一个pwd的校验规则
+        pwd:[/^[\S]{6,12}$/,'密码必须为6-12位，且不能出现空格'],
+        // 校验两次密码是否一致的规则
+        repwd:function(value){
+            // 通过形参拿到的是确认密码框中的内容
+            // 还需要拿到密码框中的内容
+            // 然后进行一次等于的判断
+            // 如果判断失败，则return一个提示消息即可
+            var pwd = $('.reg-box [name=password]').val();
+            if(pwd !== value){
+                return '两次密码不一致!'
+            }
+        }
+
+    })
+    // 监听注册表单的提交事件 http://api-breakingnews-web.itheima.net/
+    // 监听注册表单的提交事件
+    $('#form_reg').on('submit', function(e) {
+    // 1. 阻止默认的提交行为
+        e.preventDefault()
+    // 2. 发起Ajax的POST请求
+        $.ajax({
+            method:'POST',
+            url:'http://api-breakingnews-web.itheima.net/api/reguser',
+            data:$(this).serialize(),
+            success:function(res){
+                if(res.status != 0){
+                    return layer.msg(res.message)
+                }
+                layer.msg('登录成功',{time:1500},function(){
+                    localStorage.setItem('token',res.token);
+                    location.href = "index.html"
+                })
+            }
+        })
+    })
+    // 监听表单登录事件
+    $('#form_login').on('submit',function(e){
+        // 阻止默认行为
+        e.preventDefault();
+        $.ajax({
+            method:'POST',
+            url:'http://api-breakingnews-web.itheima.net/api/login',
+            data: $(this).serialize(),
+            success:function(res){
+                if(res.status != 0){
+                    return layer.msg(res.message)
+                }
+                layer.msg('登录成功',{time:1500},function(){
+                    localStorage.setItem('token',res.token);
+                    location.href = "index.html"
+                })
+            }
+        })
+    })
+})
